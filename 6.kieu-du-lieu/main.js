1e3d1930b162f5a80999d54ff545f794c966b293
thuongcoder <thuongcoder@gmail.com>
2025-11-14 12:42:07 +0700

    kieu du lieu

---
+++ 6.kieu-du-lieu/main.js
@@ -0,0 +1,9 @@
+let chuoi = "Kiểu dữ liệu chuổi";
+let so = 10;
+let sothapphan = 3.14;
+let Nam = true;
+let kieuUndefined;
+console.log(typeof so);
+console.log(typeof sothapphan);
+console.log(typeof Nam);
+console.log(typeof kieuUndefined);
