dc8f9b69161efbef7692616490982fa4d15d58fc
thuongcoder <thuongcoder@gmail.com>
2025-11-15 16:00:21 +0700

    Ép kiểu dữ liệu parseInt, parseFloat, Number

---
+++ 9.ep-kieu-dl/main.js
@@ -0,0 +1,16 @@
+//let soA = prompt("Mời nhập số");
+
+//console.log(typeof soA);
+//console.log(soA);
+//let soB = 5;
+//console.log(typeof soB);
+
+//ép kiểu dữ liệu
+
+let soA = Number(prompt("Mời nhập số A"));
+let soB = Number(prompt("Mời nhập số B"));
+
+console.log(`số A là kiểu ${typeof soA}`);
+console.log(`số B là kiểu ${typeof soB}`);
+let kq = soA + soB;
+console.log(`Kết quả A + B là: ${kq}`);
