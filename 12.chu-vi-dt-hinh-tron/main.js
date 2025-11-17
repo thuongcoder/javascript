d416c50556d98167895332a1d52bff36b66fe495
thuongcoder <thuongcoder@gmail.com>
2025-11-17 16:29:44 +0700

    Chu vi - diện tích hình tròn

---
+++ 12.chu-vi-dt-hinh-tron/main.js
@@ -0,0 +1,6 @@
+let r = Number(prompt("Nhập vào bán kính hình tròn"));
+let Pi = Math.PI;
+let dientich = r * r * Pi;
+let chuvi = r * 2 * Pi;
+console.log(`Diện tích hình tròn ${dientich}`);
+console.log(`Chu vi hình tròn ${chuvi}`);
