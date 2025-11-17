70b3cf7da153001a0cbb51aae85a6906ee8a8374
thuongcoder <thuongcoder@gmail.com>
2025-11-17 16:10:38 +0700

    Toán tử so sánh && || !

---
+++ 11.toan-tu-so-sanh/main.js
@@ -0,0 +1,13 @@
+let a = 5;
+let b = 10;
+let c = 15;
+console.log(a > b && a > c);
+console.log(a > b && a < c);
+console.log(a < b && a < c);
+
+//toan tu ||
+console.log(a > b || a > c);
+console.log(a > b || a < c);
+console.log(a < b || a < c);
+// phủ định !
+console.log(!(a < b));
