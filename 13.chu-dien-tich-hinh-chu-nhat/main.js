bcaf88726ba9d8b214fb263a23144a4a4d51dd46
thuongcoder <thuongcoder@gmail.com>
2025-11-17 16:36:19 +0700

    Chu vi - diện tích hình chữ nhật

---
+++ 13.chu-dien-tich-hinh-chu-nhat/main.js
@@ -0,0 +1,6 @@
+let dai = parseFloat(prompt("Nhập chiều dài"));
+let rong = parseFloat(prompt("Nhập chiều rộng"));
+let chuvi = (dai + rong) * 2;
+let dientich = dai * rong;
+console.log(`Chu vi hình chữ nhật ${chuvi}`);
+console.log(`Diện tích hình chữ nhật ${dientich}`);
